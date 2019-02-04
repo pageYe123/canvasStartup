b28892062875d9d3a40480451b54675839fea2fd
jeffrey <908202572@qq.com>
2019-02-04 19:49:59 +0800

    modify throttle duration

--- fireworks/js/fireworks.js
+++ fireworks/js/fireworks.js
@@ -52,7 +52,7 @@ var Fireworks = (function() {
 
     // add the canvas in
     document.body.appendChild(mainCanvas);
-    var fn = throttle(createFirework, 280);
+    var fn = throttle(createFirework, 360);
     document.addEventListener('mouseup', fn, true);
     document.addEventListener('touchend', fn, true);
 
